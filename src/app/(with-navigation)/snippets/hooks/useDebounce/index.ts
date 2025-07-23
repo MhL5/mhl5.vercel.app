7c1cff84732ea0c405b62db05ea5f980088bd64c
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-23 17:37:44 +0330

    Add useDebounce hook for debouncing values with a new implementation; include useDebounceValue for direct value debouncing and add documentation for both hooks.

---
+++ src/app/(with-navigation)/snippets/hooks/useDebounce/index.ts
@@ -0,0 +1,26 @@
+import useTimeout from "@/app/(with-navigation)/snippets/hooks/useTimeout";
+import { useEffect, useState } from "react";
+
+export function useDebounce(
+  callback: () => void,
+  delay: number,
+  dependencies: unknown[],
+) {
+  const { reset, clear } = useTimeout(callback, delay);
+  useEffect(reset, [...dependencies, reset]);
+  useEffect(clear, [clear]);
+}
+
+export function useDebounceValue<T>(inputValue: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(inputValue);
+
+  useDebounce(
+    () => {
+      setDebouncedValue(inputValue);
+    },
+    delay,
+    [inputValue],
+  );
+
+  return debouncedValue;
+}
