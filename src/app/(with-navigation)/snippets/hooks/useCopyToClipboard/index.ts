805745fea2d8fb2f910a01b978c19fda6694d0d2
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-26 02:07:12 +0330

    Update ESLint configuration to warn on console usage; refactor useCopyToClipboard hook to track copy status with hasCopied state instead of isCopying.

--- src/app/(with-navigation)/snippets/hooks/useCopyToClipboard/index.ts
+++ src/app/(with-navigation)/snippets/hooks/useCopyToClipboard/index.ts
@@ -1,20 +1,19 @@
 import { useCallback, useRef, useState } from "react";
 
 export default function useCopyToClipboard(content: string) {
-  const [isCopying, setIsCopying] = useState(false);
+  const [hasCopied, setHasCopied] = useState(false);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleCopy = useCallback(async () => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
 
     await navigator.clipboard.writeText(content);
-    setIsCopying(true);
+    setHasCopied(true);
 
     timeoutRef.current = setTimeout(() => {
-      setIsCopying(false);
-      console.log("\x1b[35m" + `is it me?` + "\x1b[0m");
+      setHasCopied(false);
     }, 2000);
   }, [content]);
 
-  return { isCopying, handleCopy };
+  return { hasCopied, handleCopy };
 }
