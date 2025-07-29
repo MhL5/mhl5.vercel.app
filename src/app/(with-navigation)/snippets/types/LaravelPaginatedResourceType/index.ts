72ff5ae4302a17efd26f4e1697f415e6538d15db
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-07-29 16:52:27 +0330

    Update LaravelPaginatedResourceType to allow 'from' and 'to' properties to be nullable; enhance type flexibility for pagination handling.

--- src/app/(with-navigation)/snippets/types/LaravelPaginatedResourceType/index.ts
+++ src/app/(with-navigation)/snippets/types/LaravelPaginatedResourceType/index.ts
@@ -2,7 +2,7 @@ export type LaravelPaginatedResource<T> = {
   current_page: number;
   data: T[];
   first_page_url: string;
-  from: number;
+  from: number | null;
   last_page: number;
   last_page_url: string;
   links: {
@@ -14,6 +14,6 @@ export type LaravelPaginatedResource<T> = {
   path: string;
   per_page: number;
   prev_page_url: string | null;
-  to: number;
+  to: number | null;
   total: number;
 };
