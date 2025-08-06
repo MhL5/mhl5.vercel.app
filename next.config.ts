b77ee979f457c9ad511b98d2f1dc58bafeda6144
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-08-06 11:17:25 +0330

    migrate: migrate ts version from 5 to 5.9

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
