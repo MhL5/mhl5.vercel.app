eb607beba11b1f76be3afac60c992a9a48ee198f
Mohammad Lashani <135700087+MhL5@users.noreply.github.com>
2025-08-31 00:32:46 +0330

    Add useCallbackRef custom hook: Introduce a new hook to convert callbacks to refs, preventing unnecessary re-renders and re-executions of effects when used as props or dependencies.

---
+++ src/hooks/useCallbackRef.ts
@@ -0,0 +1,18 @@
+import { useEffect, useMemo, useRef } from "react";
+
+/**
+ * A custom hook that converts a callback to a ref to avoid triggering re-renders when passed as a
+ * prop or avoid re-executing effects when passed as a dependency
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useCallbackRef<T extends (...args: any[]) => any>(
+  callback: T,
+): T {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  });
+
+  return useMemo(() => ((...args) => callbackRef.current?.(...args)) as T, []);
+}
