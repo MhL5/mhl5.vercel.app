4e30aefc0438e88d8f2b16cc9092254e074d2f6b
MhL5 <sapumr@gmail.com>
2025-10-23 18:40:52 +0330

    refactor: update portfolio name in manifest for branding consistency

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default async function manifest(): Promise<MetadataRoute.Manifest> {
   return {
-    name: "Mohammad Lashani Portfolio",
+    name: "MhL",
     short_name: "MhL",
     description:
       "Mohammad Lashani - Software engineer passionate about building user-friendly and efficient web applications. Explore my portfolio, code snippets, and technical insights.",
