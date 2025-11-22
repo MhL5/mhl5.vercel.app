db2aea85321179df734c5035dd0af3874e118d17
MhL5 <sapumr@gmail.com>
2025-11-22 22:45:54 +0330

    refactor: update import order in utils.ts and enhance DrawerDialog example with OctagonAlert component and improved layout

--- src/registry/new-york/DrawerDialog/example.tsx
+++ src/registry/new-york/DrawerDialog/example.tsx
@@ -1,3 +1,4 @@
+import { OctagonAlert } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { DialogClose } from "@/components/ui/dialog";
 import {
@@ -17,21 +18,26 @@ export default function Example() {
         <Button>trigger</Button>
       </DrawerDialogTrigger>
 
-      <DrawerDialogContent>
-        <DrawerDialogScrollArea className="not-sm:p-5">
-          <div className="flex w-full flex-col gap-4">
-            <DrawerDialogTitle>DrawerDialog Example</DrawerDialogTitle>
-            <DrawerDialogDescription>
-              This is a description of the DrawerDialog Example.
+      <DrawerDialogContent className="sm:max-w-md">
+        <DrawerDialogScrollArea className="grid place-items-center gap-4 not-sm:p-5 not-sm:pt-6">
+          <div className="flex size-10 items-center justify-center rounded-full bg-destructive">
+            <OctagonAlert />
+          </div>
+
+          <div className="flex w-full flex-col gap-4 text-center">
+            <DrawerDialogTitle>Are you absolutely sure?</DrawerDialogTitle>
+            <DrawerDialogDescription className="text-balance">
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
             </DrawerDialogDescription>
 
-            <div className="mt-3 flex items-center justify-end gap-3">
+            <div className="mt-3 grid grid-cols-1 items-center justify-center gap-3 sm:mx-auto sm:max-w-xs sm:grid-cols-2">
               <DrawerDialogClose asChild>
                 <Button variant="outline">Cancel</Button>
               </DrawerDialogClose>
 
               <DialogClose asChild>
-                <Button>Submit</Button>
+                <Button variant="destructive">Delete account</Button>
               </DialogClose>
             </div>
           </div>
