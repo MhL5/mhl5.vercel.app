546fa105648778aaacb0560bbc8c98d5ab2e4180
MhL5 <sapumr@gmail.com>
2025-11-28 00:18:55 +0330

    refactor: enhance AutoGrid component by improving prop documentation, simplifying styles, and removing unnecessary unique ID generation

--- src/registry/new-york/AutoGrid/AutoGrid.tsx
+++ src/registry/new-york/AutoGrid/AutoGrid.tsx
@@ -1,57 +1,69 @@
-import { type ComponentProps, useId } from "react";
-
-// Do not touch
-const cssCalculations = `
---grid-col-size-calc: calc(
-  (100% - var(--grid-gap) * var(--grid-max-col-count)) /
-    var(--grid-max-col-count)
-);
---grid-col-min-size-calc: min(
-  100%,
-  max(var(--grid-min-col-size), var(--grid-col-size-calc))
-);
-display: grid;
-gap: var(--grid-gap);
-grid-template-columns: repeat(
-  auto-fit,
-  minmax(var(--grid-col-min-size-calc), 1fr)
-);
-`;
+import type { ComponentProps } from "react";
+import { cn } from "@/lib/utils";
 
 type AutoGridProps<T extends keyof HTMLElementTagNameMap> = {
+  /**
+   * The maximum number of columns to display.
+   * @example 5
+   */
   maxColCount: number;
-  minColSize: `${number}rem` | `${number}px`;
-  gap: `${number}rem` | `${number}px`;
+  /**
+   * The minimum size of each column.
+   * `${number}rem` or `${number}px`
+   * @example "5rem" or "80px"
+   */
+  minColSize: AutoGridSize;
+  /**
+   * The gap between columns.
+   * `${number}rem` or `${number}px`
+   * @example "1rem" or "16px"
+   */
+  gap: AutoGridSize;
 
+  /**
+   * The HTML element to render.
+   * @example "div"
+   */
   as: T;
 } & ComponentProps<T>;
 
 /**
- * AutoGrid is a component that automatically adjusts the grid columns based on the screen size.
+ * automatically adjusts the grid columns without relying on breakpoints.
  */
 export default function AutoGrid<T extends keyof HTMLElementTagNameMap>({
-  gap,
-  maxColCount,
-  minColSize,
+  gap = "1rem",
+  maxColCount = 5,
+  minColSize = "5rem",
   as,
+  style,
+  className,
   ...props
 }: AutoGridProps<T>) {
   const Component = as || "div";
-  const id = useId();
 
   return (
-    <>
-      <style>
-        {`
-          #${id} {
-            --grid-max-col-count: ${maxColCount || 5};
-            --grid-min-col-size: ${minColSize || `5rem`};
-            --grid-gap: ${gap || `1rem`};
-            ${cssCalculations}
-        `}
-      </style>
-      {/* @ts-expect-error todo: temp solution */}
-      <Component id={id} {...props} />
-    </>
+    // @ts-expect-error
+    <Component
+      className={cn(
+        "grid grid-cols-[repeat(auto-fit,minmax(var(--grid-col-min-size-calc),1fr))] gap-(--grid-gap)",
+        className,
+      )}
+      style={{
+        "--grid-gap": resolveSize(gap),
+        "--grid-max-col-count": maxColCount,
+        "--grid-min-col-size": resolveSize(minColSize),
+        "--grid-col-min-size-calc":
+          "min(100%, max(var(--grid-min-col-size), var(--grid-col-size-calc)))",
+        "--grid-col-size-calc":
+          "calc((100% - var(--grid-gap) * var(--grid-max-col-count)) / var(--grid-max-col-count))",
+        ...style,
+      }}
+      {...props}
+    />
   );
 }
+
+type AutoGridSize = `${number}rem` | `${number}px`;
+
+const resolveSize = (size: AutoGridSize) =>
+  size.endsWith("px") ? `${+size.replace("px", "") / 16}rem` : size;
