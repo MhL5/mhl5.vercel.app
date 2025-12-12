768884ab3922380d34295e93be7552c60138eafc
MhL5 <sapumr@gmail.com>
2025-12-12 16:22:39 +0330

    refactor: update useMediaQuery hook to support Tailwind CSS breakpoints with improved type definitions and documentation

--- src/registry/hooks/useMediaQuery/useMediaQuery.ts
+++ src/registry/hooks/useMediaQuery/useMediaQuery.ts
@@ -5,29 +5,43 @@ import type { StringWithAutoComplete } from "@/registry/types/AutoComplete/AutoC
 
 type Direction = "max" | "min";
 
-type CssQueryString =
-  StringWithAutoComplete<`(${Direction}-width: ${number}rem)`>;
+/**
+ * Tailwind V4 Breakpoints: https://tailwindcss.com/docs/responsive-design#overview
+ */
+type TailwindCssBreakpoints = {
+  sm: "40rem"; // 640px
+  md: "48rem"; // 768px
+  lg: "64rem"; // 1024px
+  xl: "80rem"; // 1280px
+  "2xl": "96rem"; // 1536px
+};
+type TailwindCssBreakpointValues =
+  TailwindCssBreakpoints[keyof TailwindCssBreakpoints];
 
-function subscribe(cssQueryString: CssQueryString, onChange: () => void) {
-  const mediaQueryList = window.matchMedia(cssQueryString);
+/**
+ * Auto complete for tailwind css breakpoints
+ */
+type Query =
+  StringWithAutoComplete<`(${Direction}-width: ${TailwindCssBreakpointValues})`>;
+
+function subscribe(query: Query, onChange: () => void) {
+  const mediaQueryList = window.matchMedia(query);
   mediaQueryList.addEventListener("change", onChange);
   return () => mediaQueryList.removeEventListener("change", onChange);
 }
 
-function getSnapshot(cssQueryString: CssQueryString) {
-  return window.matchMedia(cssQueryString).matches;
+function getSnapshot(query: Query) {
+  return window.matchMedia(query).matches;
 }
 
 function getServerSnapshot() {
   return false;
 }
 
-function useMediaQuery(cssQueryString: CssQueryString) {
+export function useMediaQuery(query: Query) {
   return useSyncExternalStore(
-    (onChange) => subscribe(cssQueryString, onChange),
-    () => getSnapshot(cssQueryString),
+    (onChange) => subscribe(query, onChange),
+    () => getSnapshot(query),
     getServerSnapshot,
   );
 }
-
-export { useMediaQuery };
