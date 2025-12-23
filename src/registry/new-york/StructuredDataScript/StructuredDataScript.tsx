c9356b3a236fd191d57649a01c7624136432123a
MhL5 <sapumr@gmail.com>
2025-12-23 20:44:57 +0330

    refactor: clarify comment regarding the safety of dangerouslySetInnerHTML in StructuredDataScript component

--- src/registry/new-york/StructuredDataScript/StructuredDataScript.tsx
+++ src/registry/new-york/StructuredDataScript/StructuredDataScript.tsx
@@ -13,7 +13,7 @@ export default function StructuredDataScript<T extends Thing>({
     <script
       id={id}
       type="application/ld+json"
-      // biome-ignore lint/security/noDangerouslySetInnerHtml: this is safe because the data comes from our code and not from the users
+      // this is safe as long as the data comes from our code and not from the users input
       dangerouslySetInnerHTML={{
         __html: JSON.stringify(data),
       }}
