a8cdf797c2aecedd52dc9313bca22a37e0b62a37
MhL5 <sapumr@gmail.com>
2025-12-25 19:48:45 +0330

    refactor: simplify neighbor navigation in SnippetH1 component by extracting GoToNeighbor function for improved readability and maintainability

--- src/app/(with-navigation)/snippets/[...slug]/_components/SnippetH1.tsx
+++ src/app/(with-navigation)/snippets/[...slug]/_components/SnippetH1.tsx
@@ -1,10 +1,10 @@
+import SnippetBreadCrumb from "@/app/(with-navigation)/snippets/[...slug]/_components/SnippetBreadCrumb";
+import { getSnippetsLinks } from "@/app/(with-navigation)/snippets/_constants/snippetsConstants";
+import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import type { Route } from "next";
 import Link from "next/link";
 import type { ReactNode } from "react";
-import { getSnippetsLinks } from "@/app/(with-navigation)/snippets/_constants/snippetsConstants";
-import SnippetBreadCrumb from "@/app/(with-navigation)/snippets/[...slug]/_components/SnippetBreadCrumb";
-import { Button } from "@/components/ui/button";
 
 type SnippetH1Props = {
   heading: ReactNode;
@@ -24,42 +24,51 @@ export default async function SnippetH1({ heading, slug }: SnippetH1Props) {
         </h1>
 
         <div className="flex gap-2">
-          {neighbors.map((neighbor, index) => {
-            if (!neighbor)
-              return (
-                <Button
-                  key={`h1-link-button-disabled-${index + 1}`}
-                  size="icon"
-                  disabled
-                  variant="secondary"
-                  className="size-8 xl:size-9"
-                  title={`not available`}
-                >
-                  <ArrowRight className={index === 0 ? "rotate-180" : ""} />
-                </Button>
-              );
-
-            return (
-              <Button
-                key={`h1-link-button-${neighbor.title}-${index}`}
-                size="icon"
-                variant="secondary"
-                className="size-8 xl:size-9"
-                asChild
-              >
-                <Link
-                  href={(neighbor?.url as Route) || "#"}
-                  className="size-8 xl:size-9"
-                  title={`go ${index === 0 ? "previous" : "next"}`}
-                >
-                  <ArrowRight className={index === 0 ? "rotate-180" : ""} />
-                </Link>
-              </Button>
-            );
-          })}
+          <GoToNeighbor neighbor={neighbors[0]} position="previous" />
+          <GoToNeighbor neighbor={neighbors[1]} position="next" />
         </div>
       </div>
     </div>
+  );
+}
+
+function GoToNeighbor({
+  neighbor,
+  position,
+}: {
+  neighbor: Awaited<ReturnType<typeof findSnippetsNeighbors>>[number] | null;
+  position: "previous" | "next";
+}) {
+  if (!neighbor)
+    return (
+      <Button
+        size="icon"
+        disabled
+        variant="secondary"
+        className="size-8 xl:size-9"
+        title="not available"
+        aria-disabled
+      >
+        <ArrowRight className={position === "previous" ? "rotate-180" : ""} />
+      </Button>
+    );
+
+  const { title, url } = neighbor;
+  return (
+    <Button
+      size="icon"
+      variant="secondary"
+      className="size-8 xl:size-9"
+      asChild
+    >
+      <Link
+        href={url as Route}
+        className="size-8 xl:size-9"
+        title={`go to ${position === "previous" ? `previous "${title}"` : `next "${title}"`}`}
+      >
+        <ArrowRight className={position === "previous" ? "rotate-180" : ""} />
+      </Link>
+    </Button>
   );
 }
 
