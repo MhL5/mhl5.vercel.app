71db95f8469989e4416fea8bd11a35f8442aa8fb
MhL5 <sapumr@gmail.com>
2026-01-03 00:14:24 +0330

    feat: implement useThrottle hook for rate-limiting function calls and improve performance in React components

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,62 @@
+"use client";
+
+import { useMemo } from "react";
+
+import { useCallbackRef } from "./useCallbackRef";
+import { useOnUnMount } from "./useOnUnMount";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function throttle<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number,
+) {
+  let shouldWait = false;
+  let waitingArgs: Parameters<T> | null = null;
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  function timeoutCallback() {
+    if (waitingArgs === null) shouldWait = false;
+    else {
+      callback(...waitingArgs);
+      waitingArgs = null;
+      timeoutId = setTimeout(timeoutCallback, delay);
+    }
+  }
+
+  const throttled = (...args: Parameters<T>) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    callback(...args);
+    shouldWait = true;
+    timeoutId = setTimeout(timeoutCallback, delay);
+  };
+
+  throttled.cancel = () => {
+    if (timeoutId !== null) clearTimeout(timeoutId);
+    timeoutId = null;
+    shouldWait = false;
+    waitingArgs = null;
+  };
+
+  return throttled;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useThrottle<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number,
+) {
+  const callbackRef = useCallbackRef(callback);
+
+  const throttled = useMemo(
+    () => throttle(callbackRef, delay),
+    [callbackRef, delay],
+  );
+
+  useOnUnMount(() => throttled.cancel());
+
+  return throttled;
+}
