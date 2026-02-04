6712f1a7c7b9f9fedd335364d3a2ec58eb8e2bf9
MhL5 <sapumr@gmail.com>
2026-02-04 19:16:14 +0330

    refactor: enhance ThemeToggle component with keyboard shortcut and tooltip support

--- src/components/buttons/ThemeToggle.tsx
+++ src/components/buttons/ThemeToggle.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import useEventListener from "@/registry/hooks/useEventListener/useEventListener";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+
+import { Kbd } from "../ui/kbd";
+import ButtonWithTooltip from "./ButtonWithTooltip";
 
 export function ThemeToggle() {
   const { setTheme } = useTheme();
 
+  useEventListener(
+    "keypress",
+    (e) => {
+      if (e.key === "t" || e.key === "T") {
+        setTheme((theme) => (theme === "dark" ? "light" : "dark"));
+      }
+    },
+    typeof window !== "undefined" ? window : undefined,
+  );
+
   return (
-    <Button
+    <ButtonWithTooltip
       variant="ghost"
       size="icon"
+      tooltipContent={
+        <>
+          Toggle theme, Keyboard shortcut: <Kbd>T</Kbd>
+        </>
+      }
       onClick={() => setTheme((theme) => (theme === "dark" ? "light" : "dark"))}
     >
       {/* 
@@ -20,6 +38,6 @@ export function ThemeToggle() {
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 [transition-property:all!important] [transition-duration:200ms!important] dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 [transition-property:all!important] [transition-duration:200ms!important] dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
-    </Button>
+    </ButtonWithTooltip>
   );
 }
