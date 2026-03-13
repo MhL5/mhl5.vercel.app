51b4d6dae810176931ef43ea38755ef7d5c1834b
MhL5 <sapumr@gmail.com>
2026-03-14 01:51:54 +0330

    remove script

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/sonner";
 import ThemeProvider from "@/providers/ThemeProvider";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Script from "next/script";
 import type { ReactNode } from "react";
 
 import DirectionProvider from "./DirectionProvider";
@@ -14,13 +13,6 @@ type ProvidersProps = {
 export default function Providers({ children }: ProvidersProps) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        data-site-id="site_c7mmmzvdmiuh35v2"
-        src="/js/script.js"
-        defer
-      />
-
       <DirectionProvider>
         <ThemeProvider>
           <Toaster />
