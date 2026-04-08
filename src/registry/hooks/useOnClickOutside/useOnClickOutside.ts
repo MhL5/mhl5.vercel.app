a08a19a9061a8f7e6213e2235acbfa8f7bf32468
MhL5 <sapumr@gmail.com>
2026-04-09 01:34:56 +0330

    feat: remove useEventListener

--- src/registry/hooks/useOnClickOutside/useOnClickOutside.ts
+++ src/registry/hooks/useOnClickOutside/useOnClickOutside.ts
@@ -1,18 +1,24 @@
 "use client";
 
-import useEventListener from "@/registry/hooks/useEventListener/useEventListener";
-import { isBrowser } from "@/registry/utils/checks/checks";
-import type { RefObject } from "react";
+import { type RefObject, useEffect, useEffectEvent } from "react";
 
 export default function useOnClickOutside(
   ref: RefObject<HTMLElement | null>,
   cb: (e: MouseEvent) => void,
 ) {
-  useEventListener(
-    "click",
-    (e) => {
-      if (ref.current && !ref.current.contains(e.target as HTMLElement)) cb(e);
-    },
-    isBrowser() ? document : undefined,
+  const onClickEvent = useEffectEvent((e: PointerEvent) =>
+    ref.current && !ref.current.contains(e.target as HTMLElement)
+      ? cb(e)
+      : null,
   );
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    document.addEventListener("click", onClickEvent, {
+      signal: abortController.signal,
+    });
+
+    return () => abortController.abort();
+  }, [ref]);
 }
