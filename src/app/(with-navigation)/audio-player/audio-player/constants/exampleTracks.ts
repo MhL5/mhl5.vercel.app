5cc62763a4eaa6b161dfed1306f39a6e4085a0c5
MhL5 <sapumr@gmail.com>
2026-05-17 13:45:24 +0330

    feat: add typography component

--- src/app/(with-navigation)/audio-player/audio-player/constants/exampleTracks.ts
+++ src/app/(with-navigation)/audio-player/audio-player/constants/exampleTracks.ts
@@ -1,46 +1,46 @@
 export const exampleTracks = [
   {
-    name: "unknown",
+    name: "1",
     id: "1",
     url: "/temp/2_5434098755836097144.mp3",
   },
   {
-    name: "unknown",
+    name: "2",
     id: "2",
     url: "/temp/4_5810181992242745544.mp3",
   },
   {
-    name: "unknown",
+    name: "3",
     id: "3",
     url: "/temp/4_5818909872594360671.m4a",
   },
   {
-    name: "unknown",
+    name: "4",
     id: "4",
     url: "/temp/4_5855167973622290792.m4a",
   },
   {
-    name: "unknown",
+    name: "5",
     id: "5",
     url: "/temp/4_5929176328921158771.mp3",
   },
   {
-    name: "unknown",
+    name: "6",
     id: "6",
     url: "/temp/4_735364702485351829.mp3",
   },
   {
-    name: "unknown",
+    name: "7",
     id: "7",
     url: "/temp/NF_-_Remember_This.mp3",
   },
   {
-    name: "unknown",
+    name: "8",
     id: "8",
     url: "/temp/nf_chasing.mp3",
   },
   {
-    name: "unknown",
+    name: "9",
     id: "9",
     url: "/temp/remix-rusi-.mp3",
   },
---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,148 @@
+import { cn } from "@/lib/utils";
+import { type VariantProps, cva } from "class-variance-authority";
+import { type ComponentProps, type HTMLAttributes } from "react";
+
+const textSizeClassNames = {
+  xs: "gap-1 text-xs [&>svg]:mt-0.25 [&>svg:not([class*='size-'])]:size-3",
+  sm: "gap-1.5 text-sm [&>svg]:mt-0.5 [&>svg:not([class*='size-'])]:size-3.5",
+  base: "gap-2 text-base [&>svg]:mt-0.75 [&>svg:not([class*='size-'])]:size-4",
+  lg: "gap-2.5 text-lg [&>svg]:mt-1 [&>svg:not([class*='size-'])]:size-4.5",
+  xl: "gap-3 text-xl [&>svg]:mt-1.25 [&>svg:not([class*='size-'])]:size-5",
+  "2xl":
+    "gap-3.5 text-2xl [&>svg]:mt-1.5 [&>svg:not([class*='size-'])]:size-5.5",
+  "3xl": "gap-4 text-3xl [&>svg]:mt-1.75 [&>svg:not([class*='size-'])]:size-6",
+  "4xl": "gap-4.5 text-4xl [&>svg]:mt-2 [&>svg:not([class*='size-'])]:size-6.5",
+};
+
+const titleVariants = cva(
+  "font-medium [&_svg]:shrink-0 [&:has(>svg)]:inline-flex [&:has(>svg)]:items-start",
+  {
+    variants: {
+      variant: {
+        default: "",
+        withIcon: "",
+      },
+      size: {
+        default: textSizeClassNames.base,
+        md: textSizeClassNames.lg,
+        lg: textSizeClassNames.xl,
+        xl: textSizeClassNames["2xl"],
+        "2xl": textSizeClassNames["3xl"],
+      },
+    },
+    defaultVariants: {
+      size: "default",
+      variant: "default",
+    },
+  },
+);
+
+type TitleProps = {
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+} & HTMLAttributes<HTMLHeadingElement> &
+  VariantProps<typeof titleVariants>;
+
+function Title({
+  as = "h2",
+  size = "default",
+  variant = "default",
+  className,
+  ...props
+}: TitleProps) {
+  const Heading = as;
+
+  return (
+    <Heading
+      className={cn(titleVariants({ size, variant }), className)}
+      {...props}
+    />
+  );
+}
+
+const paragraphVariants = cva(
+  "font-normal text-muted-foreground [&_svg]:shrink-0 [&:has(>svg)]:inline-flex [&:has(>svg)]:items-start",
+  {
+    variants: {
+      variant: {
+        default: "",
+        withIcon: "inline-flex shrink-0 items-start",
+      },
+      size: {
+        default: textSizeClassNames.sm,
+        md: textSizeClassNames.base,
+        lg: textSizeClassNames.lg,
+        xl: textSizeClassNames.xl,
+        "2xl": textSizeClassNames["2xl"],
+      },
+    },
+    defaultVariants: {
+      size: "default",
+      variant: "default",
+    },
+  },
+);
+
+type ParagraphProps = ComponentProps<"p"> &
+  VariantProps<typeof paragraphVariants>;
+
+function Paragraph({
+  className,
+  size = "default",
+  variant = "default",
+  ...props
+}: ParagraphProps) {
+  return (
+    <p
+      className={cn(paragraphVariants({ size, variant }), className)}
+      {...props}
+    />
+  );
+}
+
+const spanVariants = cva(
+  "font-normal text-muted-foreground [&_svg]:shrink-0 [&:has(>svg)]:inline-flex [&:has(>svg)]:items-start",
+  {
+    variants: {
+      variant: {
+        default: "",
+        withIcon: "inline-flex shrink-0 items-start",
+      },
+      size: {
+        default: textSizeClassNames.xs,
+        md: textSizeClassNames.sm,
+        lg: textSizeClassNames.base,
+        xl: textSizeClassNames.lg,
+        "2xl": textSizeClassNames.xl,
+      },
+    },
+    defaultVariants: {
+      size: "default",
+      variant: "default",
+    },
+  },
+);
+
+type SpanProps = ComponentProps<"span"> & VariantProps<typeof spanVariants>;
+
+function Span({
+  className,
+  size = "default",
+  variant = "default",
+  ...props
+}: SpanProps) {
+  return (
+    <span
+      className={cn(spanVariants({ size, variant }), className)}
+      {...props}
+    />
+  );
+}
+
+export {
+  type TitleProps,
+  Title,
+  type ParagraphProps,
+  Paragraph,
+  type SpanProps,
+  Span,
+};
